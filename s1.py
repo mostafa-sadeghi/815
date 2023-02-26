524bfd22e7d75f327972a23308bc1dc5f5d52f73
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-02-26 17:43:27 +0330

    sadeghi

---
+++ s1.py
@@ -0,0 +1,9 @@
+print("hello every body")
+print("welcome to our python class")
+print("we are going to learn every things about python")
+print("python from zero to hero!!!")
+print()
+print("1+3")
+print(1+3)
+
+print('hello world')
