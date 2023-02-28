ec6c334243c566c6498809be637221ca427b7541
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-02-28 17:58:10 +0330

    sadeghi

---
+++ s4.py
@@ -0,0 +1,59 @@
+# score = 0
+# print(type(score))
+
+# x = 1.5
+# print(type(x))
+
+# نامگذاری متغیرها
+# هر ترکیبی از کاراکترهای الفبای انگلیسی، اعداد و آندرلاین
+# با این شرط که اولین کاراکتر عدد نباشد
+
+# s1 = 12
+# s_1 = 13
+
+# _s1 = 15
+
+# 1s = 15 error
+# number 1 = 15 error
+# number - one = 12 error
+
+
+# name = 'liana'
+# print(type(name))
+# name = "liana"
+# print(type(name))
+# name = ""
+# print(type(name))
+# name = "c"
+# print(type(name))
+# name = 'c'
+# print(type(name))
+# name = ''
+# print(type(name))
+
+message = "hello every body.\nwelcome to our python class.\nwe are going to learn every things about python from zero to hero!!!"
+print(message)
+message = """hello every body.
+welcome to our python class.
+we are going to learn every things about python from zero to hero!!!"""
+print(message)
+message = '''hello every body.
+welcome to our python class.
+we are going to learn every things about python from zero to hero!!!'''
+print(message)
+
+
+message = "he said:\"not do that\""
+print(message)
+message = 'he said:"not do that"'
+print(message)
+message = '''he said:"not do that"'''
+print(message)
+
+# exercise 1 : یک متغیر تعریف کنید و نام خود را در آن قرار دهید
+# متغیر را پرینت نمائید
+# یک متغیر دیگر تعریف و سن خود را در آن قرار دهید
+# متغیر دوم را نیز پرینت نمائید
+
+# exercise 2: پیغام زیر را پرینت نمایئد
+#  He said, "Aren't can't shouldn't wouldn't"
