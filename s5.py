e7cf63430d4970c5d273d65d843ee7b7565a475d
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-03-05 17:56:41 +0330

    sadeghi

---
+++ s5.py
@@ -0,0 +1,33 @@
+# name = 'alisa'
+# age = 12
+
+# message = name + ' has ' + str(age) + ' ' + 'years old.'
+# print(message)
+
+# message = "%s has %s years old" % (name, age)
+# print(message)
+
+# message = f"{name} has {age} years old"
+# print(message)
+
+# name = input('enter your name: ')
+# age = input('enter your age: ')
+# message = f"{name} has {age} years old"
+# print(message)
+
+number1 = int(input('enter first number: '))
+number2 = int(input('enter second number: '))
+
+output = number1 + number2
+
+print(f'{number1} + {number2} = {output}')
+
+
+# exercise 1 : برنامه ای بنویسید که سه عدد را از ورودی دریافت نماید و
+# عدد اول و دوم را باهم جمع نموده از عدد سوم کم کند و در نهایت حاصل را نمایش دهد.
+
+
+# Exercise 2: برنامه ای بنویسید که نام و نام خانوادگی دانش آموز را در یافت نماید و
+# پیغام زیر را نمایش دهد
+
+# ali mahdavi, welcome to our class.
