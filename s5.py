432e657116eb26f45d9290042ba36fe3e327fa69
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-03-07 17:57:33 +0330

    sadeghi

--- s5.py
+++ s5.py
@@ -15,16 +15,23 @@
 # message = f"{name} has {age} years old"
 # print(message)
 
-number1 = int(input('enter first number: '))
-number2 = int(input('enter second number: '))
+# number1 = int(input('enter first number: '))
+# number2 = int(input('enter second number: '))
 
-output = number1 + number2
+# output = number1 + number2
 
-print(f'{number1} + {number2} = {output}')
+# print(f'{number1} + {number2} = {output}')
 
 
 # exercise 1 : برنامه ای بنویسید که سه عدد را از ورودی دریافت نماید و
 # عدد اول و دوم را باهم جمع نموده از عدد سوم کم کند و در نهایت حاصل را نمایش دهد.
+
+# num1 = int(input('enter a number:> '))
+# num2 = int(input('enter a number:> '))
+# num3 = int(input('enter a number:> '))
+
+# result = num1 + num2 - num3
+# print(f'{num1} + {num2} - {num3 } = {result}')
 
 
 # Exercise 2: برنامه ای بنویسید که نام و نام خانوادگی دانش آموز را در یافت نماید و
---
+++ s6.py
@@ -0,0 +1,29 @@
+# x = 2 * 3
+
+# string = 'abc' * 3
+
+# print(string)
+
+# string1 = 'asd'
+# string2 = 'efg'
+
+# print(string1 + string2)
+
+shopping_list = ['apple', 'banana', 'bread']
+shopping_list.append('tomato')
+
+print("shopping list:", shopping_list)
+
+print("first item in shopping list is:", shopping_list[0])
+print("second item in shopping list is:", shopping_list[1])
+print("third item in shopping list is:", shopping_list[2])
+item = input('enter product name:> ')
+shopping_list.append(item)
+print("shopping list:", shopping_list)
+
+# exercise 1:
+# برنامه ای بنویسید که اسامی چهار دانش آموز را از ورودی بگیرد و
+# داخل یک لیست اضافه نماید
+# لیست را پرینت کنید
+# اولین اسم را از داخل لیست پرینت کنید
+# آخرین اسم را از داخل لیست پرینت کنید.
