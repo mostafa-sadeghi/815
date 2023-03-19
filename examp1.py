42e98f74fe6aa3e4f8ce7e97e6151c52c9a5e6c4
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-03-19 18:04:44 +0330

    sadeghi

---
+++ examp1.py
@@ -0,0 +1,71 @@
+# سوال اول
+
+# name = input("enter student's name: ")
+# family = input("enter student's family: ")
+# score1 = float(input('enter math score: '))
+# score2 = float(input('enter chem score: '))
+# score3 = float(input('enter programming score: '))
+# معدل میشه مجموع سه نمره تقسیم بر تعدادشون
+
+# total = score1 + score2 + score3
+# print(f"{name} {family}'s total score is : {total}")
+# average = total/3
+# print(f"{name} {family}'s average is : {average}")
+
+# سوال دوم
+
+# name = input('enter your name:> ')  # barad
+# print(name[4])
+# print(name[3])
+# print(name[2])
+# print(name[1])
+# print(name[0])
+
+
+# name = input('enter your name:> ')  # elay
+# print(name[3])
+# print(name[2])
+# print(name[1])
+# print(name[0])
+
+
+# name = input('enter your name:> ')  # liana
+# print(name[4])
+# print(name[3])
+# print(name[2])
+# print(name[1])
+# print(name[0])
+# name = input('enter your name:> ')  # sadegh
+# print(name[5])
+# print(name[4])
+# print(name[3])
+# print(name[2])
+# print(name[1])
+# print(name[0])
+
+
+# سوال شماره 3
+
+
+students = []
+
+name1 = input("enter student's name:> ")
+students.append(name1)
+
+name2 = input("enter student's name: ")
+students.append(name2)
+
+name3 = input("enter student's name: ")
+students.append(name3)
+
+name4 = input("enter student's name: ")
+students.append(name4)
+
+name5 = input("enter student's name: ")
+students.append(name5)
+
+
+print("students list :", students)
+
+print(students[0])
+print(students[4])
