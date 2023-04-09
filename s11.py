aa5b9b5c7a555fb3cf1f448b04e7db0513a293ec
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-04-09 17:58:32 +0330

    sadeghi

---
+++ s11.py
@@ -0,0 +1,61 @@
+# x = 1
+
+# x = x + 1
+
+# print(x)
+
+# print(1+2)
+# print(1*2)
+# print(1-2)
+# print(1/2)
+# print(3//2)
+
+# print(4 % 2)
+
+
+# a = 4
+# b = 5
+
+# if a < b:
+#     print("a is less than b")
+#     print("ok")
+
+# print("outside")
+
+# if a > b:
+#     print("a is greater than b")
+
+
+# a = 4
+# b = 4
+
+# if a <= b:
+#     print("a is less than or equal to b")
+#     print("ok")
+
+# print("outside")
+
+# if a >= b:
+#     print("a is greater than or equal to b")
+
+# if a == b:
+#     print("a is equal to b")
+
+# if a != b:
+#     print('a and b is not equal')
+
+
+n1 = int(input('enter a number:> '))
+n2 = int(input('enter a number:> '))
+n3 = int(input('enter a number:> '))
+
+max = 0
+if n1 > max:
+    max = n1
+if n2 > max:
+    max = n2
+if n3 > max:
+    max = n3
+print("max is:", max)
+
+# الگوریتم، فلوچارت و برنامه ای بنویسید که سه عدد را از ورودی دریافت نماید و حاصل جمع آن ها را پرینت کند
