48397d1053c71cf16cec035f27eaf56a5ac3afd5
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-04-11 17:57:38 +0330

    sadeghi

---
+++ s12.py
@@ -0,0 +1,77 @@
+# a = 2
+# b = 3
+# c = 1
+
+# if a < b and a < c:
+#     print("a in the lowest")
+
+
+# if a < b or a < c:
+#     print("a is less than b or c")
+
+
+# a = True
+# if a:
+#     print("a")
+# a = False
+
+# if not a:
+#     print("a")
+
+
+# name = input('enter a name: ')
+# score1 = float(input('enter math score: '))
+# score2 = float(input('enter programming score: '))
+
+# average = (score1 + score2)/2
+
+# if average >= 90:
+#     print(f"{name}'s average is : {average} and his/her grade: A")
+
+# elif average >= 80:
+#     print(f"{name}'s average is : {average} and his/her grade: B")
+# elif average >= 70:
+#     print(f"{name}'s average is : {average} and his/her grade: C")
+# else:
+#     print(f"{name}'s average is : {average} and his/her grade: F")
+
+
+# for i in range(5):
+#     print("I want to learn pygame.")
+#     print("I want to learn django.")
+
+# print("outside")
+
+# for i in range(5):
+#     print(i)
+# exercise 1
+# با کمک حلقه فور پنج عدد از وروردی در یافت نمائید و حاصل جمع اعداد زوج وارد شده را پرینت
+
+# for i in range(1,11):
+#     print(i)
+
+# for i in range(2, 11, 2):
+#     print(i)
+# exercise 2: با کمک حلقه فور اعداد فرد کمتر از 10 را پرینت نمائید
+
+# for i in range(10,0,-1):
+#     print(i)
+
+# for n in 'benita':
+#     print(n)
+
+# for x in [1,2,3,4,5,6]:
+#     print(x)
+
+
+# for i in range(3):
+#     print("a")
+
+# for j in range(3):
+#     print("b")
+
+# exercise 3: چرا؟
+for i in range(3):
+    print("a")
+    for j in range(3):
+        print("b")
