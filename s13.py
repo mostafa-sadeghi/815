dc63e349bb2a2217f3ebfd1fb37f958e6b5538c3
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-04-16 18:58:45 +0330

    sadeghi

--- s13.py
+++ s13.py
@@ -24,8 +24,76 @@ if
 +  الگوریتم و فلوچارت
 '''
 
-done = False
-while not done:
-    quit = input('Do you want to quit?("y" or "n") ')
-    if quit == "y":
-        done = True
+# done = False
+# while not done:
+#     quit = input('Do you want to quit?("y" or "n") ')
+#     if quit == "y":
+#         done = True
+
+
+# import pygame
+
+# pygame.init()
+
+# screen = pygame.display.set_mode((700, 500))
+# RED = (255,0,0)
+# pygame.draw.polygon(screen, RED, [[0,0],[100,0],[100,100],[0,100]])
+# pygame.display.flip()
+# done = True
+# while done:
+#     for event in pygame.event.get():
+#         if event.type == pygame.QUIT:
+#             done = False
+# با کمک حلقه فور  و وایل شکل های زیر را در ترمینا پرینت نمائید
+# exercise 1:
+'''
+* * * * * * * * * * 
+'''
+
+# exercise 2:
+'''
+* * * * * * * * * * 
+* * * * * 
+* * * * * * * * * * * * * * * * * * * *
+'''
+# exercise 3:
+'''
+* * * * * * * * * * 
+* * * * * * * * * * 
+* * * * * * * * * * 
+* * * * * * * * * * 
+* * * * * * * * * * 
+* * * * * * * * * * 
+* * * * * * * * * * 
+* * * * * * * * * * 
+* * * * * * * * * * 
+* * * * * * * * * * 
+
+'''
+# exercise 4:
+'''
+0 1 2 3 4 5 6 7 8 9
+0 1 2 3 4 5 6 7 8 9
+0 1 2 3 4 5 6 7 8 9
+0 1 2 3 4 5 6 7 8 9
+0 1 2 3 4 5 6 7 8 9
+0 1 2 3 4 5 6 7 8 9
+0 1 2 3 4 5 6 7 8 9
+0 1 2 3 4 5 6 7 8 9
+0 1 2 3 4 5 6 7 8 9
+0 1 2 3 4 5 6 7 8 9
+
+
+'''
+# exercise 5:
+'''
+0 0 0 0 0 0 0 0 0 0
+1 1 1 1 1 1 1 1 1 1
+2 2 2 2 2 2 2 2 2 2
+3 3 3 3 3 3 3 3 3 3
+4 4 4 4 4 4 4 4 4 4 
+5 5 5 5 5 5 5 5 5 5
+.
+.
+9 9 9 9 9 9 9 9 9 9 
+'''
