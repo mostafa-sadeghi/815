9e1d311f32760c9eca4b0726d73c28fb3388c20c
msa <msai.sadehi@gmail.com>
2023-06-06 20:08:32 +0330

    sadeghi

---
+++ s25.py
@@ -0,0 +1,9 @@
+# numbers = [22,6,89,0,1]
+
+# new_number = int(input('enter a number: '))
+# numbers.append(new_number)
+
+# number_to_delete = int(input('enter a number: '))
+# numbers.remove(number_to_delete)
+
+# print(numbers)
